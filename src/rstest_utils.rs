90fa2c0894011b0a16f4fcc6f5bd6db05a283389
Tomas Miklovic <tommket@gmail.com>
2022-11-29 19:17:25 +0100

    Fix formatting issue.

--- src/rstest_utils.rs
+++ src/rstest_utils.rs
@@ -1,7 +1,10 @@
 use chrono::NaiveDate;
 use rstest::fixture;
 
-use crate::{viewed_date::{DayNumber, MonthNumber, YearNumber}, utils::from_ymd};
+use crate::{
+    utils::from_ymd,
+    viewed_date::{DayNumber, MonthNumber, YearNumber},
+};
 
 #[fixture(year = 1990, month = 1, day = 1)]
 pub fn create_date(year: YearNumber, month: MonthNumber, day: DayNumber) -> NaiveDate {
